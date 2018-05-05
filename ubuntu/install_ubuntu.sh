f9dbbd6a7becace8adeac05f0ff590133acb8a7c
Dawid Laszuk <laszukdawid@gmail.com>
2018-05-04 18:45:28 -0700

    Ubuntu installation with icon.

---
+++ ubuntu/install_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+chmod +x cracker
+cp papuga.png ~/.local/share/icons/
+
+# These two require root permission.
+# TODO: Install it locally
+cp cracker.desktop /usr/share/applications/
+cp cracker /usr/bin/
