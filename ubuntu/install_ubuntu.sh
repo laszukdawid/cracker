0118f0d46141edae2855aba9456792c5bd9dd72e
Dawid Laszuk <laszukdawid@gmail.com>
2018-07-15 09:25:48 -0700

    Update Ubuntu install script

--- ubuntu/install_ubuntu.sh
+++ ubuntu/install_ubuntu.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+install_dir=$(pwd)
+echo "Install dir $install_dir"
+
+cd ..
+echo "cd $(pwd)" > cracker
+echo "python3 -m aws_polly_gui.main" >> cracker
+
 chmod +x cracker
-cp papuga.png ~/.local/share/icons/
+
 
 # These two require root permission.
 # TODO: Install it locally
-cp cracker.desktop /usr/share/applications/
-cp cracker /usr/bin/
+mv cracker /usr/bin/
+cp $install_dir/cracker.desktop /usr/share/applications/
+cp $install_dir/papuga.jpeg ~/.local/share/icons/
