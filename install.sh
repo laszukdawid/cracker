1be6fd5b46deacfe30aedcc909f74adae17adaeb
Dawid Laszuk <laszukdawid@gmail.com>
2018-08-27 22:29:19 +0200

    Agree on Ubuntu default installs

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 # Ubuntu specific installation
-apt install pyqt5-dev
-apt install python3-pyqt5
-apt install python3-pyqt5.qtmultimedia
-apt install vlc
-apt install libvlc-dev
+apt install -y pyqt5-dev
+apt install -y python3-pyqt5
+apt install -y python3-pyqt5.qtmultimedia
+apt install -y vlc
+apt install -y libvlc-dev
 
-apt install gstreamer1.0-qt5
-apt install gstreamer1.0-plugins-bad
-apt install libgstreamer-plugins-bad1.0-dev
-apt install libqt5multimedia5-plugins
+apt install -y gstreamer1.0-plugins-bad
+apt install -y gstreamer1.0-qt5
+apt install -y gstreamer1.0-nice
+apt install -y libgstreamer-plugins-bad1.0-dev
+apt install -y libqt5multimedia5-plugins
+apt install -y libqt5gstreamer-dev
+apt install -y libqt5gstreamer-1.0-0
 
+echo "\n\n\n\n"
+echo "Remember to remove ~/.local/.../PyQt5*"
