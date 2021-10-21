c658fa88bfecccb6ca9a783d4813fb1370cf6815
Dawid Laszuk <laszukdawid@gmail.com>
2021-10-20 19:59:06 -0700

    Update Ubuntu installation script

--- ubuntu/install_ubuntu.sh
+++ ubuntu/install_ubuntu.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
-set -x
+set -e
 INSTALL_DIR=$(pwd)
 echo "Install dir $INSTALL_DIR"
 
 cd ..
-echo "cd $(pwd)" > cracker
-echo "python3 -m cracker.main" >> cracker
+if [[ ! -d .venv ]] && [[ ! -d .env ]] && [[ -z ${FORCE} ]]
+then
+    echo "You likely haven't created virtual environment for this package."
+    echo "I know this because there isn't .venv nor .env directory."
+    echo "If I'm incorrect then use FORCE=true flag, i.e."
+    echo "\`FORCE=true bash install_ubuntu.sh\`"
+    exit 0
+fi
 
-chmod +x cracker
+cat << EOF > cracker_tmp
+cd $(pwd)
+.venv/bin/python -m cracker.main
+EOF
+
+chmod +x cracker_tmp
 
 
 # These two require root permission.
-# TODO: Install it locally
-mv cracker /usr/bin/
-cp $INSTALL_DIR/cracker.desktop /usr/share/applications/
-cp $INSTALL_DIR/papuga.jpeg ~/.local/share/icons/
+cp $INSTALL_DIR/cracker.desktop ~/.local/share/applications/
+# sudo cp $INSTALL_DIR/icon.jpeg ~/.local/share/icons/cracker.jpeg
+cp $INSTALL_DIR/icon.png ~/.local/share/icons/cracker.png
+sudo mv cracker_tmp /usr/bin/cracker
