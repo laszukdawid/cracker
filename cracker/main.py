889822bf4ee0577016f661f0b566da5090ace831
Dawid <git@dawid.lasz.uk>
2023-03-12 22:08:42 -0700

    Enable dark/light mode in-sync with gnome and windows

--- cracker/main.py
+++ cracker/main.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python
-# coding: UTF-8
 import sys
+import threading
 
+import darkdetect
+from PyQt5.QtCore import QFile, Qt, QTextStream
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon
 
+import cracker.themes
 from cracker.cracker import Cracker
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("Cracker")
+
+    # Force the style to be the same on all OSs:
+    app.setStyle("Fusion")
+
+    def toggle_pallet(theme: str):
+        if theme.lower() == "dark":
+            file = QFile(":/dark/stylesheet.qss")
+        else:
+            file = QFile(":/light/stylesheet.qss")
+        file.open(QFile.ReadOnly | QFile.Text)
+        stream = QTextStream(file)
+        app.setStyleSheet(stream.readAll())
+
+    toggle_pallet(darkdetect.theme())
+    t = threading.Thread(target=darkdetect.listener, args=(toggle_pallet,))
+    t.daemon = True
+    t.start()
 
     cracker = Cracker(app)
     cracker.run()
